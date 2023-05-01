0f50dc23e3b49095c8670014670647cb8cd77d9b
Mike2611 <mpuentes2611@gmail.com>
2023-05-01 17:36:21 -0600

    Add boostrap 4 and add script Jquery

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import '@/assets/css/global.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import BootstrapVue from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 library.add(fas);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
