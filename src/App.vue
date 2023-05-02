f3f2e2fe4b130f2b8e1fc67b14078c86cdae3807
MaoRusk <manolo_rod.98@hotmail.com>
2023-05-01 22:52:21 -0600

    Proveedores y Contratos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view></router-view>
   </div>
 </template>
 
@@ -8,18 +8,17 @@
 
 export default {
   name: 'App',
-  components: {
-  }
+  components: { }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/ProvModule.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="container-fluid">
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Proveedores</h1>
+            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                    class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+        </div>
+
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <!-- <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6> -->
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Raz&oacute;n Social</th>
+                                <th>RFC</th>
+                                <th>Tel&eacute;fono</th>
+                                <th>Correo</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>Id</th>
+                                <th>Raz&oacute;n Social</th>
+                                <th>RFC</th>
+                                <th>Tel&eacute;fono</th>
+                                <th>Correo</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            <tr>
+                                <td>001</td>
+                                <td>Tiger Nixon</td>
+                                <td>QWER879810ASA</td>
+                                <td>8113848573</td>
+                                <td>tigerNixon@hotmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>002</td>
+                                <td>Tiger Nixon</td>
+                                <td>QWER879810ASA</td>
+                                <td>8113848573</td>
+                                <td>tigerNixon@hotmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>003</td>
+                                <td>Tiger Nixon</td>
+                                <td>QWER879810ASA</td>
+                                <td>8113848573</td>
+                                <td>tigerNixon@hotmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>004</td>
+                                <td>Tiger Nixon</td>
+                                <td>QWER879810ASA</td>
+                                <td>8113848573</td>
+                                <td>tigerNixon@hotmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>005</td>
+                                <td>Tiger Nixon</td>
+                                <td>QWER879810ASA</td>
+                                <td>8113848573</td>
+                                <td>tigerNixon@hotmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>006</td>
+                                <td>Tiger Nixon</td>
+                                <td>QWER879810ASA</td>
+                                <td>8113848573</td>
+                                <td>tigerNixon@hotmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>007</td>
+                                <td>Tiger Nixon</td>
+                                <td>QWER879810ASA</td>
+                                <td>8113848573</td>
+                                <td>tigerNixon@hotmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>008</td>
+                                <td>Tiger Nixon</td>
+                                <td>QWER879810ASA</td>
+                                <td>8113848573</td>
+                                <td>tigerNixon@hotmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>009</td>
+                                <td>Tiger Nixon</td>
+                                <td>QWER879810ASA</td>
+                                <td>8113848573</td>
+                                <td>tigerNixon@hotmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>010</td>
+                                <td>Tiger Nixon</td>
+                                <td>QWER879810ASA</td>
+                                <td>8113848573</td>
+                                <td>tigerNixon@hotmail.com</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
