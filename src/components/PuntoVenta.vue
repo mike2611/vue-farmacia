e526631b965dca42771540647088f89b336daeb0
Mike2611 <mpuentes2611@gmail.com>
2023-05-18 09:12:40 -0600

    Add comportamiento de scroll

--- src/components/PuntoVenta.vue
+++ src/components/PuntoVenta.vue
@@ -1,121 +1,201 @@
 <template>
     <div>
-    <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
-            <!-- Main Content -->
-            <div id="content">
-                <!-- Begin Page Content -->
-                <div class="d-flex justify-content-center">
-                    <div class="mt-5" style="width: 80vw">
-                        <div class="d-flex flex-row align-items-center justify-content-around">
-                            <div>
-                                <h6 class="blue-color font-weight-bold">Seleccionar Producto: </h6>
-                                <div class="input-group rounded">
-                                    <input type="search" class="form-control rounded" placeholder="Buscar producto ..." aria-label="Search" aria-describedby="search-addon" />
-                                    <span class="input-group-text border-0" id="search-addon">
-                                        <i class="fas fa-search"></i>
-                                    </span>
-                                </div>
-
-                                <ul class="list-group m-3">
-                                    <li v-for="(producto, index) in productos" 
-                                        :key="producto.nombre" 
-                                        class="list-group-item"
-                                        role="button"
-                                        :class="{ active: selectedProductIndex === index}" 
-                                        @click="selectProduct(producto, index)"
-                                        >
-                                        {{producto.nombre}} 
-                                        <small>{{producto.descripcion}}</small>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div class="d-flex flex-column" style="width: 15%;">
-                                <h6 class="blue-color font-weight-bold">Cantidad de {{selectedProductName ? selectedProductName : "este producto"}}: </h6>
-                                <input  v-model.number="selectedProductQuantity"  class="m-3" type="number" placeholder="0">
-                                <h6 class="blue-color font-weight-bold">Precio Unitario: </h6>
-                                <input class="m-3" type="text" :value="selectedProductPrice | formatoMoneda " readonly>
-                                <button type="button" class="btn btn-primary btn-sm" @click="añadirProducto()">Añadir producto</button>
-                            </div>
-
-                            <div>
-                                <div class="ml-4">
-                                    <span class="blue-color font-weight-bold">{{cuenta.length > 0 ? "Cuenta Actual:" : "Aún no agrega ningún producto a su cuenta" }}</span>
-                                    <div class="d-flex">
-                                        <ul class="list-group m-3">
-                                            <li v-for="producto in cuenta" :key="producto.nombre" class="list-group-item">
-                                                {{producto.nombre}} 
-                                                <small> {{producto.descripcion}}</small>
-                                            </li>                                            
-                                        </ul>
-                                        <ul class="list-group m-3">
-                                            <li  v-for="producto in cuenta" :key="producto.nombre" class="list-group-item">
-                                                {{producto.cantidad}} unidade(s) X {{producto.precio_venta | formatoMoneda }}
-                                            </li>                                
-                                        </ul>
-                                        <ul class="list-group m-3">
-                                            <li  v-for="producto in cuenta" :key="producto.nombre" class="list-group-item">
-                                                {{producto.cantidad * producto.precio_venta | formatoMoneda }}
-                                            </li>                                
-                                        </ul>
-                                    </div>
-                                    <div v-if="cuenta.length > 0">
-                                        <div class="d-flex flex-row justify-content-end m-3">
-                                            <div class="d-flex flex-column align-items-end">
-                                                <div class="d-flex justify-content-between w-100">
-                                                    <b class="mt-2">SubTotal:</b>
-                                                    <b class="mt-2 black-color">{{subtotal | formatoMoneda }}</b>
-                                                </div>
-                                                <div class="d-flex justify-content-between w-100">
-                                                    <b class="mt-2">Descuento(s):</b>
-                                                    <b class="mt-2 black-color">{{descuentos | formatoMoneda }}</b>
-                                                </div>
-                                                <div class="d-flex justify-content-between w-100">
-                                                    <b class="mt-2">IVA:</b>
-                                                    <b class="mt-2 black-color">{{iva | formatoMoneda }}</b>
-                                                </div>
-                                                <div class="d-flex justify-content-between w-100">
-                                                    <b class="mt-2 blue-color">Total:</b>
-                                                    <b class="mt-2 black-color">{{total | formatoMoneda }}</b>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        
-                                        <div class="d-flex flex-row-reverse mb-5 mr-3">
-                                            <button type="button" class="ml-2 btn btn-primary btn-sm" @click="generaTicketPDF">Pagar</button>
-                                            <button type="button" class="ml-2 btn btn-danger btn-sm" @click="vaciarCuenta">Limpiar Todo</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+      <!-- Content Wrapper -->
+      <div id="content-wrapper" class="d-flex flex-column">
+        <!-- Main Content -->
+        <div id="content">
+          <!-- Begin Page Content -->
+          <div class="d-flex justify-content-center">
+            <div class="mt-5" style="width: 80vw">
+              <div class="d-flex flex-row align-items-center justify-content-around">
+                <div>
+                  <h6 class="blue-color font-weight-bold">Seleccionar Producto: </h6>
+                  <div class="input-group rounded">
+                    <input
+                      type="search"
+                      class="form-control rounded"
+                      placeholder="Buscar producto ..."
+                      aria-label="Search"
+                      aria-describedby="search-addon"
+                    />
+                    <span class="input-group-text border-0" id="search-addon">
+                      <i class="fas fa-search"></i>
+                    </span>
+                  </div>
+  
+                  <div class="list-wrapper">
+                    <ul class="list-group m-3" v-if="productos.length <= 5">
+                      <li
+                        v-for="(producto, index) in productos"
+                        :key="producto.nombre"
+                        class="list-group-item"
+                        role="button"
+                        :class="{ active: selectedProductIndex === index }"
+                        @click="selectProduct(producto, index)"
+                      >
+                        {{ producto.nombre }}
+                        <small>{{ producto.descripcion }}</small>
+                      </li>
+                    </ul>
+                    <div class="list-scroll" v-else>
+                      <ul class="list-group m-3">
+                        <li
+                          v-for="(producto, index) in productos"
+                          :key="producto.nombre"
+                          class="list-group-item"
+                          role="button"
+                          :class="{ active: selectedProductIndex === index }"
+                          @click="selectProduct(producto, index)"
+                        >
+                          {{ producto.nombre }}
+                          <small>{{ producto.descripcion }}</small>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+  
+                <div class="d-flex flex-column" style="width: 15%;">
+                  <h6 class="blue-color font-weight-bold">
+                    Cantidad de {{ selectedProductName ? selectedProductName : "este producto" }}:
+                  </h6>
+                  <input
+                    v-model.number="selectedProductQuantity"
+                    class="m-3"
+                    type="number"
+                    placeholder="0"
+                  />
+                  <h6 class="blue-color font-weight-bold">Precio Unitario: </h6>
+                  <input
+                    class="m-3"
+                    type="text"
+                    :value="selectedProductPrice | formatoMoneda"
+                    readonly
+                  />
+                  <button
+                    type="button"
+                    class="btn btn-primary btn-sm"
+                    @click="añadirProducto()"
+                  >
+                    Añadir producto
+                  </button>
+                </div>
+  
+                <div>
+                  <div class="ml-4">
+                    <span class="blue-color font-weight-bold"
+                      >{{ cuenta.length > 0
+                        ? "Cuenta Actual:"
+                        : "Aún no agrega ningún producto a su cuenta" }}</span
+                    >
+                    <div class="d-flex">
+                      <ul class="list-group m-3">
+                        <li
+                          v-for="producto in cuenta"
+                          :key="producto.nombre"
+                          class="list-group-item"
+                          role="button"
+                          :class="{ active: selectedProductIndex === index }"
+                          @click="selectProduct(producto, index)"
+                        >
+                          {{ producto.nombre }}
+                          <small> {{ producto.descripcion }}</small>
+                        </li>
+                      </ul>
+                      <ul class="list-group m-3">
+                        <li
+                          v-for="producto in cuenta"
+                          :key="producto.nombre"
+                          class="list-group-item"
+                          role="button"
+                          :class="{ active: selectedProductIndex === index }"
+                          @click="selectProduct(producto, index)"
+                        >
+                          {{ producto.cantidad }} unidade(s) X
+                          {{ producto.precio_venta | formatoMoneda }}
+                        </li>
+                      </ul>
+                      <ul class="list-group m-3">
+                        <li
+                          v-for="producto in cuenta"
+                          :key="producto.nombre"
+                          class="list-group-item"
+                          role="button"
+                          :class="{ active: selectedProductIndex === index }"
+                          @click="selectProduct(producto, index)"
+                        >
+                          {{ producto.cantidad * producto.precio_venta | formatoMoneda }}
+                        </li>
+                      </ul>
+                    </div>
+                    <div v-if="cuenta.length > 0">
+                      <div class="d-flex flex-row justify-content-end m-3">
+                        <div class="d-flex flex-column align-items-end">
+                          <div class="d-flex justify-content-between w-100">
+                            <b class="mt-2">SubTotal:</b>
+                            <b class="mt-2 black-color">{{ subtotal | formatoMoneda }}</b>
+                          </div>
+                          <div class="d-flex justify-content-between w-100">
+                            <b class="mt-2">Descuento(s):</b>
+                            <b class="mt-2 black-color">{{ descuentos | formatoMoneda }}</b>
+                          </div>
+                          <div class="d-flex justify-content-between w-100">
+                            <b class="mt-2">IVA:</b>
+                            <b class="mt-2 black-color">{{ iva | formatoMoneda }}</b>
+                          </div>
+                          <div class="d-flex justify-content-between w-100">
+                            <b class="mt-2 blue-color">Total:</b>
+                            <b class="mt-2 black-color">{{ total | formatoMoneda }}</b>
+                          </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Abandonar sesion</h5>
-                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">×</span>
+                      </div>
+  
+                      <div class="d-flex flex-row-reverse mb-5 mr-3">
+                        <button
+                          type="button"
+                          class="ml-2 btn btn-primary btn-sm"
+                          @click="generaTicketPDF"
+                        >
+                          Pagar
                         </button>
+                        <button
+                          type="button"
+                          class="ml-2 btn btn-danger btn-sm"
+                          @click="vaciarCuenta"
+                        >
+                          Limpiar Todo
+                        </button>
+                      </div>
                     </div>
-
-                    <div class="modal-body">Si tiene una venta en proceso se perdera si sale</div>
-
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
-                        <a class="btn btn-primary" href="login.html">Salir</a>
-                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
+  
+      <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Abandonar sesión</h5>
+              <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">×</span>
+              </button>
+            </div>
+            <div class="modal-body">Si tiene una venta en proceso, se perderá si sale.</div>
+            <div class="modal-footer">
+              <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+              <a class="btn btn-primary" href="login.html">Salir</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
+  
+  
   
 <script>
 
@@ -132,6 +212,9 @@
                 {nombre: 'Paracetamol', descripcion: '500mgs', precio_venta: 100},
                 {nombre: 'Aspirina', descripcion: '500mgs', precio_venta: 100},
                 {nombre: 'Complejo B', descripcion: '500mgs', precio_venta: 100},
+                {nombre: 'Complejo A', descripcion: '500mgs', precio_venta: 150},
+                {nombre: 'Complejo C', descripcion: '500mgs', precio_venta: 200},
+
             ],
             selectedProductIndex: null,
             selectedProductPrice: null,
@@ -179,9 +262,7 @@
 
             }
         },
-        generaTicketPDF() {
-        
-            const formatoMoneda = this.$options.filters.formatoMoneda;
+        generaTicketPDF() {      
 
             const docDefinition = {
             content: [
@@ -193,15 +274,15 @@
                 
                 // Contenido de los productos en la cuenta
                 { text: 'Cuenta Actual:', style: 'subheader' },
-                { ul: this.cuenta.map(producto => `${producto.nombre} - ${producto.cantidad} unidades x ${producto.precio_venta | formatoMoneda}`) },
+                { ul: this.cuenta.map(producto => `${producto.nombre} - ${producto.cantidad} unidades x $ ${producto.precio_venta }`) },
                 { text: ' ', style: 'subheader' },
                 
                 // Totales
                 { text: 'Totales:', style: 'subheader' },
-                { text: `SubTotal: ${this.subtotal | formatoMoneda}`, style: 'total' },
-                { text: `Descuento(s): ${this.descuentos | formatoMoneda}`, style: 'total' },
-                { text: `IVA: ${this.iva | formatoMoneda}`, style: 'total' },
-                { text: `Total: ${this.total | formatoMoneda}`, style: 'total' }
+                { text: `SubTotal: $ ${this.subtotal}`, style: 'total' },
+                { text: `Descuento(s): $ ${this.descuentos}`, style: 'total' },
+                { text: `IVA: $ ${this.iva}`, style: 'total' },
+                { text: `Total: $ ${this.total}`, style: 'total' }
             ],
             styles: {
                 header: { fontSize: 18, bold: true, alignment: 'center' },
@@ -248,7 +329,15 @@
     }
     .black-color {
         color: black;
-    }/*
+    }
+    .list-scroll {
+        height: 250px;
+        overflow-y: scroll;
+    }
+    .list-wrapper {
+        max-height: 270px;
+    }
+        /*
     .center-top {
         position: absolute;
         top: 8%;
